824459cbf27bb29d112b282193933d53b6a02dbe
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-03-27 18:02:51 -0600

    Use custom pipe: external-image-url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { PokeNavigationComponent } from './poke-navigation/poke-navigation.component';
 import { PokeCatalogComponent } from './poke-catalog/poke-catalog.component';
+import { ExternalImageURLPipe } from "./pipes/external-image-url.pipe";
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent, PokeNavigationComponent, PokeCatalogComponent ],
+  declarations: [ AppComponent, PokeNavigationComponent, PokeCatalogComponent, ExternalImageURLPipe ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
