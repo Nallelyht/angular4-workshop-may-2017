9a773aa5d5a7f6d88cb7791c22111c85b554053a
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-03-28 08:00:34 -0600

    Add example of filtering method for pokemon catalog

--- src/app/poke-catalog/poke-catalog.component.ts
+++ src/app/poke-catalog/poke-catalog.component.ts
@@ -9,12 +9,14 @@ import { PokeCatalogService } from "./poke-catalog.service";
   styleUrls: [ "app/poke-catalog/poke-catalog.component.css" ]
 } )
 export class PokeCatalogComponent implements OnInit {
+  originalPokemonList:Array<Pokemon> = [];
   pokemonList:Array<Pokemon> = [];
   searchValue:string = "";
 
   @Input() set searchValueInput( value:string ) {
     console.log( "Input for search: " + value );
     this.searchValue = value;
+    this.filterPokemon();
   };
 
   constructor( private _pokeCatalogService:PokeCatalogService ) {
@@ -23,6 +25,14 @@ export class PokeCatalogComponent implements OnInit {
 
   public ngOnInit() {
     this.pokemonList = this._pokeCatalogService.getPokemonList();
+    this.originalPokemonList = this._pokeCatalogService.getPokemonList();
     console.log( this.pokemonList );
+  }
+
+  filterPokemon() {
+    this.pokemonList = this.originalPokemonList.filter( ( pokemon, index ) => {
+      // If the value to search is a substring of the pokemon name; then the pokemonList will have the current pokemon
+      return pokemon.name.includes( this.searchValue );
+    } );
   }
 }
